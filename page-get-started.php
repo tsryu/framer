03fae3f46392b5b9a2a629a06476da844640de88
Tae-seok Ryu <ryu@slowalk.co.kr>
2017-03-19 18:19:51 +0900

    get started - intro html add

--- page-get-started.php
+++ page-get-started.php
@@ -15,7 +15,7 @@ while ( have_posts() ) : the_post();
 		<section ng-repeat="post in posts | reverse" id="{{post.slug}}" class="section" ng-bind-html-unsafe="getContent(post)">
 			<h1 class="section-title">{{post.title.rendered}}</h1>
 			<div class="section-content" nag-prism ng-bind-html="post.content.rendered | html"></div>
-			<div class="edit-link text-right"><a class="post-edit-link" href="/wp-admin/post.php?post={{post.id}}&amp;action=edit">편집</a></div>
+			<?php if(is_user_logged_in()):?><div class="edit-link text-right"><a class="post-edit-link" href="/wp-admin/post.php?post={{post.id}}&amp;action=edit">편집</a></div><?php endif; ?>
 		</section>
 	</div>
 	<?php 
